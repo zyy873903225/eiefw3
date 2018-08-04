e0467c54a4778543bb13ccf2a447e4a8214bca3f
zyy873903225 <873903225@qq.com>
2018-08-04 15:51:50 +0800

    修改参数使其正常运行

--- nRF51422/application/anttt.c
+++ nRF51422/application/anttt.c
@@ -35,8 +35,8 @@ static u8 Anttt_au8SpiReceiveBuffer[U8_SPI0_BUFFER_SIZE];
 
 
 static u8 Anttt_au8TestResponse[] = {NRF_SYNC_BYTE, NRF_CMD_TEST_RESPONSE_LENGTH, NRF_CMD_TEST_RESPONSE};
-static u8 Anttt_au8AckMessage[]   = {NRF_SYNC_BYTE, ANTTT_APP_MESSAGE_ACK_LENGTH, ANTTT_APP_MESSAGE_ACK};
-static u8 Anttt_au8NAckMessage[]  = {NRF_SYNC_BYTE, ANTTT_APP_MESSAGE_NACK_LENGTH, ANTTT_APP_MESSAGE_NACK};
+static u8 Anttt_au8AckMessage[]   = {NRF_SYNC_BYTE, ANTTT_APP_MESSAGE_ACK_LENGTH, ANTTT_APP_MSG_ACK};
+static u8 Anttt_au8NAckMessage[]  = {NRF_SYNC_BYTE, ANTTT_APP_MESSAGE_NACK_LENGTH, ANTTT_APP_MSG_NACK};
 
 
 /**********************************************************************************************************************
@@ -144,6 +144,7 @@ static void AntttSM_Idle(void)
 {
   static bool bBlinkOn = true;
   u8 u8SpiMsgLength;
+  static u8 u8ReceivedBytes = 0;
   
   /* Check if a BLE module is connected to client */
   if (G_u32BPEngenuicsFlags == _BPENGENUICS_CONNECTED)
